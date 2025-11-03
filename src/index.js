303fac8ff26f1218f52d1cd0c7008be9793f99bc
azath4u <azath4u@gmail.com>
2025-11-03 18:06:19 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 //import App from './App';
-import App from './aggrid';
+import App from './aggrid.js';
 //import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
